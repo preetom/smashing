b5ad8d1d06d9fc77cfceeab263ac16a1874686a6
Preetom Banerjee <preetom.banrjee@dennislabs.com>
2019-07-29 17:41:06 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>
+      HEllo
+      <br />Love pizza
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
